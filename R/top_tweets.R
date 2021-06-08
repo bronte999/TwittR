cd05afe11bfae758d2b40fd72cd90caf41758e33
bronte999 <isabella.rahs@gmail.com>
2021-06-08 01:40:40 -0700

    Initial Commit - top_tweets functional

---
+++ R/top_tweets.R
@@ -0,0 +1,48 @@
+# top_tweets_interval
+#
+#
+#' @import tidyverse
+#' @import lubridate
+#' @param data Twitter API User Timeline  data frame
+#' @param num_tweets how many tweets to include (ex: top 10 tweets)
+#' @param time_start start of time interval investigating
+#' @param time_end end of time interval investigating
+#'
+#' @return A data frame consisting of highest liked & retweeted tweets
+#' @export
+
+
+top_tweets_interval <- function(data, num_tweets, time_start, time_end){
+  time_frame <- interval(time_start, time_end)
+  top_x_tweets <- data %>%
+    filter(created_at %within% time_frame) %>%
+    mutate(success_score = retweet_count + favorite_count) %>%
+    arrange(desc(success_score))
+
+
+  return(top_x_tweets)
+}
+
+
+# top_tweets_since_today
+#
+#
+#' @import tidyverse
+#' @import lubridate
+#' @param data Twitter API User Timeline  data frame
+#' @param num_tweets how many tweets to include (ex: top 10 tweets)
+#' @param n_days tweets from the past n_days
+#'
+#' @return A data frame consisting of highest liked & retweeted tweets
+#' @export
+
+top_tweets_since_today <- function(data, num_tweets, n_days){
+  time_frame <- interval(time_end - n_days, time_end)
+  top_x_tweets <- data %>%
+    filter(created_at %within% time_frame) %>%
+    mutate(success_score = retweet_count + favorite_count) %>%
+    arrange(desc(success_score))
+
+
+  return(top_x_tweets)
+}
