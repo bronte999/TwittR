aba330b069ed6c4f85fa1bb38482c5fb57fa1d87
Nathan Lee <nathan.l.lee123@gmail.com>
2021-06-09 21:22:02 -0700

    fixed bug in post_ratios

--- R/post_ratios.R
+++ R/post_ratios.R
@@ -8,24 +8,25 @@
 #' @export
 
 
-
 posting_ratios <- function(data){
   user_og_tweets <- data[data$is_retweet==FALSE, ]
-
+  
   user_og_tweets<- subset(user_og_tweets, is.na(user_og_tweets$reply_to_status_id))
-
+  
   user_retweets <- data[data$is_retweet==TRUE,]
-
+  
   user_replies <- subset(data, !is.na(data$reply_to_status_id))
-
+  
   post_data <- data.frame(
     category=c("Original", "Retweets", "Replies"),
     count=c(nrow(user_og_tweets), nrow(user_retweets), nrow(user_replies)))
-
-  post_data$fraction = data$count / sum(data$count)
-  post_data$percentage = data$count / sum(data$count) * 100
-  post_data$ymax = cumsum(data$fraction)
-  post_data$ymin = c(0, head(data$ymax, n=-1))
-
+  print(post_data$count / sum(post_data$count))
+  post_data$fraction = post_data$count / sum(post_data$count)
+  
+  post_data$percentage = post_data$count / sum(post_data$count) * 100
+  post_data$ymax = cumsum(post_data$fraction)
+  post_data$ymin = c(0, head(post_data$ymax, n=-1))
+  
   return(post_data)
 }
+
